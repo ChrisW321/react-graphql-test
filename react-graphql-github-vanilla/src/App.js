67e56ee271cf9b7dd9a487682f1a02b55001faa2
Chris W <christopherkwildenradt@gmail.com>
2018-10-19 12:59:04 -0700

    Add repository and issue components

--- react-graphql-github-vanilla/src/App.js
+++ react-graphql-github-vanilla/src/App.js
@@ -7,29 +7,57 @@ const TITLE = 'React GraphQL GitHub Client';
 const axiosGitHubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    Authorization: `bearer ${process.env.REACT_APP_API}`,
   },
 });
 
-const GET_ORGANIZATION = `{
-  organization(login: "the-road-to-learn-react") {
-    name
-    url
+const getIssuesOfRepositoryQuery = (organization, repository) => `
+  {
+    organization(login: "${organization}") {
+      name
+      url
+      repository(name: "${repository}") {
+        name
+        url
+        issues(last: 5) {
+          edges {
+            node {
+              id
+              title
+              url
+            }
+          }
+        }
+      }
+    }
   }
-}`
+`;
 
 class App extends Component {
   state = {
     path: 'the-road-to-learn-react/the-road-to-learn-react',
+    organization: null,
+    errors: null,
   }
 
   componentDidMount() {
-    this.onFetchFromGithub();
+    this.onFetchFromGithub(this.state.path);
   }
 
-  onFetchFromGithub() {
-    axiosGitHubGraphQL.post('', { query: GET_ORGANIZATION })
-    .then(res => console.log(res));
+  onFetchFromGithub(path) {
+    const [organization, repository] = path.split('/');
+
+    axiosGitHubGraphQL.post('', { 
+      query: getIssuesOfRepositoryQuery(organization, repository), 
+    })
+    .then(res => {
+      this.setState({
+        organization: res.data.data.organization,
+        errors: res.data.errors,
+      });
+      console.log(res.data.data)
+    })
+    .catch(err => console.error(err));
   }
 
   onChange = e => {
@@ -38,11 +66,11 @@ class App extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    //fetch data
+    this.onFetchFromGithub(this.state.path)
   }
 
   render() {
-    const { path } = this.state;
+    const { path, organization } = this.state;
     return (
       <div>
         <h1>{TITLE}</h1>
@@ -63,11 +91,36 @@ class App extends Component {
 
         <hr />
 
-        {/* Here comes the result! */}
+        {organization && <Organization organization={organization} />}
       </div>
     );
   }
 }
 
+const Organization = ({ organization }) => (
+  <div>
+    <p>
+      <strong>Issues from Organization:</strong>
+      <a href={organization.url}>{organization.name}</a>
+    </p>
+    <Repository repository={organization.repository} />
+  </div>
+);
+
+const Repository = ({ repository }) => (
+  <div>
+    <p>
+      <strong>In Repository:</strong>
+      <a href={repository.url}>{repository.name}</a>
+    </p>
+    <ul>
+      {repository.issues.edges.map(issue => (
+        <li key={issue.node.id}>
+          <a href={issue.node.url}>{issue.node.title}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default App;
